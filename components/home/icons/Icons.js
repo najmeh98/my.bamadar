7e76af26d43d4a71ee84b6500d49b55a89b70f91
najmeh98 <foroughi.najmeh98@gmail.com>
2021-12-12 10:54:05 +0330

    Added Icon

--- components/home/icons/Icons.js
+++ components/home/icons/Icons.js
@@ -223,7 +223,7 @@ export const AddIcon = () => (
 );
 
 export const TrushICon = ({ fill }) => (
-  <svg
+  <Svg
     className="MuiSvgIcon-root"
     focusable="false"
     viewBox="0 0 24 24"
@@ -233,11 +233,11 @@ export const TrushICon = ({ fill }) => (
     fill={fill}
   >
     <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5z"></path>
-  </svg>
+  </Svg>
 );
 
 export const AddblueIcon = ({ fill }) => (
-  <svg
+  <Svg
     className="MuiSvgIcon-root"
     focusable="false"
     viewBox="0 0 24 24"
@@ -246,11 +246,11 @@ export const AddblueIcon = ({ fill }) => (
     fill={fill}
   >
     <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
-  </svg>
+  </Svg>
 );
 
 export const DecreaseIcon = ({ fill }) => (
-  <svg
+  <Svg
     className="MuiSvgIcon-root"
     focusable="false"
     viewBox="0 0 24 24"
@@ -259,7 +259,7 @@ export const DecreaseIcon = ({ fill }) => (
     fill={fill}
   >
     <path d="M19 13H5v-2h14v2z"></path>
-  </svg>
+  </Svg>
 );
 
 export const ArrowIcon = () => {
