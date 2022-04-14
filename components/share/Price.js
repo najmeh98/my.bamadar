657b9a1b1fe13ca4d125f1215c2556868f233ef4
najmeh98 <foroughi.najmeh98@gmail.com>
2022-04-14 19:43:31 +0430

    update price

--- components/share/Price.js
+++ components/share/Price.js
@@ -13,7 +13,7 @@ export const Price = ({ children, cross }) => {
 };
 
 const numberWithCommas = (x) => {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x && x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 console.log(numberWithCommas(123345678));
 
