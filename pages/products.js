b3afb8888b0eb59370a59a5be40f85c3c386cc95
najmeh98 <foroughi.najmeh98@gmail.com>
2022-10-22 22:06:43 +0330

    change url config

--- pages/products.js
+++ pages/products.js
@@ -38,7 +38,7 @@ const Products = () => {
     //   const subProduct = [];
     if (!id) return;
     axios
-      .get(`${config}/data/api/product/${id}`)
+      .get(`${config}/api/product/${id}`)
       .then(function (response) {
         console.log(response.data.data);
         SetsubProduct(response.data.data);
